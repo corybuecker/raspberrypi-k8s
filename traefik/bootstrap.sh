4af6a996b4f5777582c2156776c96a73dd659830
Cory Buecker <email@corybuecker.com>
2025-03-23 11:19:06 -0500

    feat: add prometheus (#3)

--- traefik/bootstrap.sh
+++ traefik/bootstrap.sh
@@ -8,4 +8,4 @@ export IFS=$'\n\t'
 
 helm repo add traefik https://traefik.github.io/charts
 helm repo update
-helm upgrade --namespace kube-system --install traefik traefik/traefik -f routing.yaml
+helm upgrade --namespace kube-system --install -f routing.yaml traefik traefik/traefik
